cf228b39d7fce4c031b59f6e111a24b88cf82d75
Matthias <matthias.moulin@gmail.com>
2015-04-24 10:00:35 +0200

    Update

--- src/invariantdistribution.py
+++ src/invariantdistribution.py
@@ -4,6 +4,9 @@ from scipy.stats import rv_discrete
 
 np.random.seed(87655678)
 
+############################################################################### 
+# TESTS
+###############################################################################
 def get_T1():
     return np.array([[0.9, 0.075, 0.025], [0.15, 0.8, 0.05], [0.25, 0.25, 0.5]])
 
@@ -19,22 +22,14 @@ def get_M2():
 def get_M3():
     return get_M(0.001, 0.5)
     
-def invariant_distribution(T):
-    E, V = np.linalg.eig(T)
-    I = None
-    for i in range(len(E)):
-        if (abs(E[i]-1.0) < 10**-7):
-            if (I != None):
-                raise Error('Multiple eigenvalues are equal to 1.0')
-            else:
-                I = V[:,i]
-        elif (E[i] > 1.0):
-            raise Error('Eigenvalue ' + str(E[i]) + ' > 1.0')
-    if (I == None):
-        raise Error('No eigenvalue is equal to 1.0')
-    else:
-        return normalise1(I)
-
+def test():
+    approximation_error_walkers(T=get_M1(), walkers=1000, steps=100, norm=norm2)
+    approximation_error_walkers(T=get_M2(), walkers=1000, steps=100, norm=norm2)
+    approximation_error_walkers(T=get_M3(), walkers=1000, steps=100, norm=norm2)
+    
+############################################################################### 
+# NORMS
+############################################################################### 
 def norm2(v):
     return np.linalg.norm(v)
     
@@ -53,21 +48,9 @@ def normalise2(v):
 def normalise1(v):
     return normalise(v, norm1)
 
-def direct_simulation(T, steps, X0=0):
-    X = np.zeros((steps+1), dtype=int)
-    X[0] = X0
-    values = range(T.shape[0])
-    for k in range(1, steps+1):
-        X[k] = discrete_sample(values, T[:,X[k-1]])
-    return X
-
-def invariant_distribution_via_direct_simulation(T, walkers, steps):
-    invd = np.zeros(T.shape[0], dtype=int)
-    for w in range(walkers+1):
-        i = direct_simulation(T=T, steps=steps, X0=np.random.choice(range(T.shape[0])))[steps]
-        invd[i] = invd[i] + 1
-    return np.float64(invd) / np.float64(walkers)
-       
+############################################################################### 
+# DISCRETE-STATE DISCRETE-TIME MARKOV PROCESSES
+###############################################################################  
 def discrete_sample(values, probabilities, ns=1):
     distrib = rv_discrete(values=(range(len(values)), probabilities))
     indices = distrib.rvs(size=ns)
@@ -75,29 +58,55 @@ def discrete_sample(values, probabilities, ns=1):
         return map(values.__getitem__, indices)[0]
     else:
         return map(values.__getitem__, indices) 
+
+def direct_simulation(T, steps, X0=0):
+    X = np.zeros((steps+1), dtype=int)
+    X[0] = X0
+    values = range(T.shape[0])
+    for k in range(1, steps+1):
+        X[k] = discrete_sample(values, T[:,X[k-1]])
+    return X
+    
+############################################################################### 
+# INVARIANT DISTRIBUTION
+############################################################################### 
+def invariant_distribution(T):
+    E, V = np.linalg.eig(T)
+    I = None
+    for i in range(len(E)):
+        if (abs(E[i]-1.0) < 10**-7):
+            if (I != None):
+                raise Error('Multiple eigenvalues are equal to 1.0')
+            else:
+                I = V[:,i]
+        elif (E[i] > 1.0):
+            raise Error('Eigenvalue ' + str(E[i]) + ' > 1.0')
+    if (I == None):
+        raise Error('No eigenvalue is equal to 1.0')
+    else:
+        return normalise1(I)
+
+def invariant_distribution_via_direct_simulation(T, walkers, steps):
+    invd = np.zeros(T.shape[0], dtype=int)
+    for w in range(walkers):
+        i = direct_simulation(T=T, steps=steps, X0=np.random.choice(range(T.shape[0])))[steps]
+        invd[i] = invd[i] + 1
+    return np.float64(invd) / np.float64(walkers)
         
-def approximation_error(T):
+def approximation_error_walkers(T, walkers, steps, norm=norm2):
     I = invariant_distribution(T)
-    ns = range(100,1001,100)
-    ews = np.zeros(10)
-    i = 0
-    for w in ns:
-        Iw = invariant_distribution_via_direct_simulation(T, walkers=w, steps=100)
-        ews[i] = norm2(I-Iw)
-        i = i + 1
-    ess = np.zeros(10)   
-    i = 0
-    for s in ns:
-        Is = invariant_distribution_via_direct_simulation(T, walkers=100, steps=s)
-        ess[i] = norm2(I-Is)
-        i = i + 1
+    ews = np.zeros((walkers), dtype=np.float64)
+    invd = np.zeros(T.shape[0], dtype=int)
+    for w in range(walkers):
+        i = direct_simulation(T=T, steps=steps, X0=np.random.choice(range(T.shape[0])))[steps]
+        invd[i] = invd[i] + 1
+        ews[w] = norm(I - (np.float64(invd) / np.float64(w+1)))
         
     plt.figure()
-    plt.plot(ns, ews)
-    plt.plot(ns, ess)
-    plt.legend(loc=1)
+    plt.plot(range(1, walkers+1), ews)
+    plt.xlabel('Walkers')
     plt.ylabel('Error')
-    plt.show() 
+    plt.show()
 
 class Error(Exception):
     def __init__(self, msg=None):
